02ca5cc5776c8a93ac1c9fe3d1e96cb54f84e490
Stephen Smith <blackrim@gmail.com>
2012-03-13 14:49:54 -0400

    first attempt

---
+++ simple_add.py
@@ -0,0 +1,40 @@
+import newick3,phylo3,sys
+import networkx as nx
+import matplotlib.pyplot as plt
+import simple_combine
+
+def get_node_leaves(graph):
+    node_leaves = {}
+    for i in graph.nodes():
+        if len(graph.successors(i)) == 0:
+            node_leaves[i] = [i]
+        else:
+            stack = [i]
+            node_leaves[i] = []
+            while len(stack) != 0:
+                lab = stack.pop()
+                if len(graph.successors(lab)) == 0:
+                    node_leaves[i].append(lab)
+                else:
+                    for j in graph.successors(lab):
+                        stack.append(j)
+    return node_leaves
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print "python simple_add.py tree1 in.dot out.dot"
+        sys.exit(0)
+    infile = open(sys.argv[1],"r")
+    intree1 = newick3.parse(infile.readline())
+    infile.close()
+
+    graph = nx.read_dot(sys.argv[2])
+    for i in graph.edges():
+        graph[i[0]][i[1]]['weight'] = int(graph[i[0]][i[1]]['weight'])
+    node_leaves = get_node_leaves(graph)
+
+    graph = simple_combine.combine_tree(graph,node_leaves,intree1)
+    pos=nx.graphviz_layout(graph)
+    nx.draw(graph,pos)
+    plt.show()
+    nx.write_dot(graph,sys.argv[3])
---
+++ simple_combine.py
@@ -0,0 +1,140 @@
+import newick3,phylo3,sys
+import networkx as nx
+import matplotlib.pyplot as plt
+
+def get_leaf_names(lvs):
+    ret = []
+    for i in lvs:
+        ret.append(i.label)
+    return ret
+
+def process_tree_to_graph(tree):
+    graph = nx.DiGraph()
+    node_leaves = {}
+    count = 0
+    for i in tree.iternodes(order=0):
+        nd = None
+        ed = None
+        if i.parent == None:
+            i.label = str(count)
+            count += 1
+            nd = i.label
+            node_leaves[nd] = get_leaf_names(i.leaves())
+            graph.add_node(nd)
+        elif i.label != None:
+            nd = i.label
+            node_leaves[nd] = get_leaf_names(i.leaves())
+            graph.add_node(nd)
+            graph.add_edge(i.parent.label,nd,weight = 1)
+        else:
+            i.label = str(count)
+            count += 1
+            nd = i.label
+            node_leaves[nd] = get_leaf_names(i.leaves())
+            graph.add_node(nd)
+            graph.add_edge(i.parent.label,nd,weight =1)
+#    nx.draw_graphviz(graph)
+#    plt.show()
+    return graph,node_leaves
+
+def get_graph_edge(node_leaves,lvs,plvs,names_not_in_tree,names_not_in_graph):
+    parent = None
+    child = None
+    for i in node_leaves:
+        if (set(lvs)-names_not_in_graph) == (set(node_leaves[i])-names_not_in_tree):
+            child = i
+    for i in node_leaves:
+        if (set(plvs)-names_not_in_graph) == (set(node_leaves[i])-names_not_in_tree):
+            parent = i
+    return parent,child
+
+def combine_tree(graph,node_leaves,tree):
+ #   print graph.nodes()
+    gnames = []
+    new_names = [] #names not yet really in the graph
+    names_not_in_tree = []
+    maxcount = 0
+    tree_leaf_names = get_leaf_names(tree.leaves())
+    for i in graph.nodes():
+ #       print i,graph.out_edges(i),graph.in_degree(i),graph.successors(i),graph.predecessors(i)
+        if len(graph.successors(i)) == 0:
+            gnames.append(i)
+        else: #just getting max internal node count
+            if int(i) >= maxcount:
+                maxcount  = int(i)+1
+    names_not_in_tree = set(set(gnames) - set(tree_leaf_names))
+ #   print gnames
+ #   print maxcount
+    for i in tree.leaves():
+        if i.label not in gnames:
+            nd = i.label
+            graph.add_node(nd)
+            node_leaves[nd] = get_leaf_names(i.leaves())
+            new_names.append(nd)
+    new_names = set(new_names)
+    for i in tree.iternodes(order=1):
+        if i.parent == None:
+            continue
+        else:
+#            print i.label,i.parent.label
+            lvs = get_leaf_names(i.leaves())
+            plvs = get_leaf_names(i.parent.leaves())
+            parent,child = get_graph_edge(node_leaves,lvs,plvs,names_not_in_tree,new_names)
+            if len(i.children) == 0:
+                child = i.label
+            print parent,child
+            #add if parent and child are disconnected (multiple nodes in between)
+            #add if child isn't there
+            #add if non overlapping taxa
+            if parent and child:
+                try:
+                    graph[parent][child]['weight'] += 1
+                except:
+                    graph.add_edge(parent,child,weight=1)
+                    print "adding edge",parent,child
+            elif parent == None and child:
+                i.parent.label = str(maxcount)
+                maxcount += 1
+                graph.add_node(i.parent.label)
+                print "adding node",i.parent.label
+                graph.add_edge(i.parent.label,child,weight = 1)
+                print "adding edge",i.parent.label,child
+                node_leaves[i.parent.label] = get_leaf_names(i.parent.leaves())
+#                sys.exit(0)
+            elif parent == None and child == None:
+                if len(i.children) == 0:
+                    child = i.label
+                else:
+                    i.label = str(maxcount)
+                    child = i.label
+                    maxcount += 1
+                    graph.add_node(child)
+                    print "adding node",child
+                    node_leaves[child] = get_leaf_names(i.leaves())
+                i.parent.label = str(maxcount)
+                maxcount += 1
+                graph.add_node(i.parent.label)
+                print "adding node",i.parent.label
+                graph.add_edge(i.parent.label,child,weight = 1)
+                print "adding edge",i.parent.label,child
+                node_leaves[i.parent.label] = get_leaf_names(i.parent.leaves())
+    return graph
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print "python simple_combine.py tree1 tree2 out.dot"
+        sys.exit(0)
+    infile = open(sys.argv[1],"r")
+    intree1 = newick3.parse(infile.readline())
+    infile.close()
+    graph1,node_leaves1 = process_tree_to_graph(intree1)
+    infile = open(sys.argv[2],"r")
+    intree2 = newick3.parse(infile.readline())
+    infile.close()
+    graph2,node_leaves2 = process_tree_to_graph(intree2)
+
+    graph = combine_tree(graph1,node_leaves1,intree2)
+    pos=nx.graphviz_layout(graph)
+    nx.draw(graph,pos)
+    plt.show()
+    nx.write_dot(graph,sys.argv[3])
