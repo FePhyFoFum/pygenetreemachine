fd41816472fa270eaa582a6f790b9e5f6d1d3784
Stephen Smith <blackrim@gmail.com>
2012-03-16 14:50:13 -0400

    added simple make graph script

---
+++ src/make_graph.py
@@ -0,0 +1,16 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import sys
+import simple_combine
+import newick3,phylo3
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print "python make_graph.py in.tre out.dot"
+        sys.exit(0)
+
+    infile = open(sys.argv[1],"r")
+    intree1 = newick3.parse(infile.readline())
+    infile.close()
+    ingraph,node_leaves_in = simple_combine.process_tree_to_graph(intree1,1000)
+    nx.write_dot(ingraph,sys.argv[2])
